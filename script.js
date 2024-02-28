f89eecac05f366ea22a5c99e829e89b7f8e8bbdf
Himanush <himanushchintu@gmail.com>
2024-02-28 17:50:25 +0530

    completed js functions

--- script.js
+++ script.js
@@ -20,4 +20,37 @@ function addTask(){
     }
     //after the value is added the input box should be empty
     inputBox.value = "";
+    // whenever any task is added we need to call the saveData inorder to save the new data in the browser. 
+    saveData();
 }
+
+listContainer.addEventListener("click",function(e){
+    // when clicked, it checks where we have clicked and if clicked target element is LI then it should add checked classname,if already checked classname is already there then it will remove the checked as toggle is added.
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked"); 
+        // will call saveData when we check or uncheck the task
+        saveData(); 
+    }
+    // if clicked target element is span it will delete the parent element.The parent element is li element.So it'll remove the li element.So the task will be deleted.
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        // will call saveData after deleting a task to update the data.
+        saveData();
+    }
+},false);
+
+function saveData(){
+    // setItem stores the data entered in the browser with the name "data" and to get the data we can use getItem i.e localStorage.getItem("data").
+    // The first parameter is the name and second parameter is value we want to save.here we want to save entire content in list container.
+    // listContainer.innerHTML - gives whatever data is present in the list container.
+      localStorage.setItem("data",listContainer.innerHTML);
+}
+
+// To display data whenever we open the website again
+
+function showTask(){
+    // This will give all the content that is stored in browser with name "data".
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
